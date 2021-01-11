6de4ade85cf2e52b9aab4291cdb17fa938b78ca3
lixiaobiao <2946571671@>
2021-01-11 11:13:36 +0800

    动态创建9.png图片

---
+++ app/src/main/java/com/example/jetpackdemo/pagerMore/viewPager2AndViewpager2/ViewPager2Activity.java
@@ -0,0 +1,16 @@
+package com.example.jetpackdemo.pagerMore.viewPager2AndViewpager2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.jetpackdemo.R;
+
+public class ViewPager2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager2);
+    }
+}
